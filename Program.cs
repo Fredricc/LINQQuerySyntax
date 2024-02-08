1af8ac8c7d7b02fb22621c7592abaae7cd14efc7
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-02-08 20:04:51 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LINQQuerySyntax
+{
+    public record Employee(string FirstName, string LastName, string Department);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = new List<Employee>
+            {
+                new Employee("Nicholas", "Mamau", "Development"),
+                new Employee("Ken", "Njuguna", "Sales"),
+                new Employee("Ken", "Njenga", "Sales"),
+                new Employee("Paul", "Kiprotech", "Project Management"),
+                new Employee("Isaac", "Soita","Project Management"),
+                new Employee("James", "Kimani", "Development")
+            };
+
+            var selectedEmployees = from employee in employees
+                                    where employee.FirstName.StartsWith("J")
+                                    orderby employee.LastName
+                                    select employee;
+
+            foreach (var employee in selectedEmployees)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName}");
+            }
+
+            
+        }
+    }
+}
